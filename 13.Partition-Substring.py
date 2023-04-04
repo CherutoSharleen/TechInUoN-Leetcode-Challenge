44f33f83eafca4939fa1146ee602aec9a89b06b4
Cheruto <cheruto@students.uonbi.ac.ke>
2023-04-04 23:51:47 +0300

    Partition string into unique substrings

---
+++ 13.Partition-Substring.py
@@ -0,0 +1,31 @@
+'''
+Our task is to partition the string into one or more substrings such that the characters
+in each substring are unique.
+
+We have to return the minimum number of substrings in such a partition.
+
+1. No letter can be repeated in a substring
+
+s = abacaba
+output = 4
+Explanation: The string can be partitioned as [a, ba, cab, a] and [ab,ac,ab,a]
+'''
+
+# Solution
+class Solution:
+    def partitionString(self, s: str) -> int:
+        lastSeen = [-1]*26
+        count = 1
+        substringStarting = 0
+
+        for i in range(len(s)):
+            if lastSeen[ord(s[i]) - ord('a')] >= substringStarting:
+                count += 1
+                substringStarting = i
+            lastSeen[ord(s[i]) - ord('a')] = i
+
+        return count
+
+sol = Solution()
+print(sol.partitionString('abacaba'))
+        
